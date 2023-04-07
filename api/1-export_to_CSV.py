ec538f2e30c5becfce877f3a782ddbbb57b52c84
Redginald Godeau <113888771+RedginaldGodeau@users.noreply.github.com>
2023-04-07 20:44:03 +0200

    Create 1-export_to_CSV.py

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+    Python script to export data in the CSV format
+"""
+import json
+import sys
+
+args = sys.argv
+
+
+def GetAPI(url=None):
+    """
+     Get data from API URL. 
+     This function is used to get data from the API URL.
+     If you want to get data from a different URL you can use this function.
+
+     @param url - URL of the API.
+     Default is None in which case None is returned
+
+     @return JSON string of the
+    """
+    from urllib.request import urlopen
+
+    # Returns the URL or None if the url is None.
+    # Returns the URL or None if the url is None.
+    if url is None:
+        return None
+
+    with urlopen(url) as response:
+        body = response.read()
+    return body
+
+
+# Summary of Employee is done.
+# This function is called from the main module.
+if __name__ == '__main__':
+    # ARGV (For UserID)
+    UserId = sys.argv[1]
+
+    # API URLS
+    UserUrl = UserUrl = "https://jsonplaceholder.typicode.com/users/{}"
+    ToDoUrl = "https://jsonplaceholder.typicode.com/todos?userId={}"
+
+    # * UserData (LISTE USER DATA MATCH WITH USERID)
+    # * ToDoData (LISTE TODATA DATA MATCH WITH USERID)
+    UserData = json.loads(GetAPI(UserUrl.format(UserId)))
+    ToDoData = json.loads(GetAPI(ToDoUrl.format(UserId)))
+
+    Username = UserData.get("name", "Guest")
+
+    csvstr = ""
+    # This function will generate CSV file for ToDoData
+    for value in ToDoData:
+        Title = value.get("title", "no name")
+        IsCompleted = value.get("completed", False)
+        csvstr += '"{}","{}","{}","{}"\n'.format(
+            UserId, Username, IsCompleted, Title)
+
+    with open("{}.csv".format(UserId), "w") as file:
+        file.write(csvstr)
